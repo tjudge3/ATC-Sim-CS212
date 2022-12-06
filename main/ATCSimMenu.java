0da84dab36bf7e46750c771a178a0b69d0314a91
tjudge3 <67015636+tjudge3@users.noreply.github.com>
2022-12-06 08:00:28 -0500

    Update ATCSimMenu.java

--- main/ATCSimMenu.java
+++ main/ATCSimMenu.java
@@ -1,65 +1,30 @@
 package main;
 
-import java.util.Scanner;
-
-//breakout the console menu system into its own file
-public class ATCSimMenu extends ATCSim {
-
-    static double meanArrivalFreq = 0.0;
-    static double meanDepartureFreq = 0.0;
-    static double simulationTime = 0.0;
-    static ATCSim sim = new ATCSim();
-
-
-    public static void simParameters() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to Air Traffic Control Simulator\n");
-        System.out.println("Enter mean departure frequency (0.0 < df > 1.0): ");
-        if (scanner.hasNextDouble()) {
-            meanDepartureFreq = scanner.nextDouble();
-            scanner.nextLine();
-        }
-        System.out.println("Enter mean arrival frequency   0.0 < af > 1.0): ");
-        if (scanner.hasNextDouble()) {
-            meanArrivalFreq = scanner.nextDouble();
-            scanner.nextLine();
-        }
-        System.out.println("Enter the simulation time in minutes (use 720 for 24hrs): ");
-        if (scanner.hasNextDouble()) {
-            simulationTime = scanner.nextDouble();
-            scanner.nextLine();
-        }
-        //  Each i in the loop represents a minute, 720 x 2 = 1440 minutes in 24 hours
-        for (int i = 0; i < simulationTime; i++) {
-            sim.processArrival(meanArrivalFreq);
-            sim.processDeparture(meanDepartureFreq);
-        }
+enum FlightType {Arrival, Departure};
+class Flight {
+    String flightNumber;
+    FlightType flightType;
+    int minuteInQueue;
+    int minuteOutQueue;
+    // constructor
+    public Flight(String flightNumber, FlightType flightType){
+        this.flightNumber = flightNumber;
+        this.flightType = flightType;
     }
-
-    public static void printSimStats() {
-        sim.printSimSummaryStatistics();
+    @Override
+    public String toString(){
+        return "main.Flight " + flightType + ": " + flightNumber;
     }
-
-    public static void printSimStatsFile() {
-        sim.printSimSummaryStatisticsToFile();
-        System.out.println("Goodbye.");
-        System.exit(0);
+    //  "minute" that flight entered the queue
+    public void setMinuteInQueue(int minute){
+        this.minuteInQueue = minute;
     }
-
-    public static void firstMenu(){
-        System.out.println("main.ATC Menu\n");
-        System.out.println("======================================");
-        System.out.println("|     Please select an option:      |");
-        System.out.println("======================================");
-        System.out.println("| Options:                           |");
-        System.out.println("|   [I] Input Parameters             |");
-        System.out.println("|   [D] Display Stats in Console     |");
-        System.out.println("|   [S] Save Stats to File           |");
-        System.out.println("|   [Q] Quit                         |");
-        System.out.println("======================================");
+    // "minute" that flight exits the queue
+    // difference is time in queue
+    public void setMinuteOutQueue(int minute){
+        this.minuteOutQueue = minute;
     }
-
+    public int timeInQueue(){
+        return minuteOutQueue - minuteInQueue;
+    }
 }
-
-
-
